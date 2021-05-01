b176e3fdd4d361a8cbc5a0215f7b5d870867b5b7
Mohd Hafiz <xmhafiz@gmail.com>
2021-05-02 02:14:40 +0800

    init project

---
+++ InstaLayoutExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  InstaLayoutExample
+//
+//  Created by Hafiz on 02/05/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ InstaLayoutExample/InstaLayoutExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaLayoutExampleApp.swift
+//  InstaLayoutExample
+//
+//  Created by Hafiz on 02/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct InstaLayoutExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
